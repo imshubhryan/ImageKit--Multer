8faf416324470b6be4bc319eb455d37c945135d1
Shubham Ryan <140340367+imshubhryan@users.noreply.github.com>
2026-07-16 18:44:37 +0530

    Add image upload functionality in controller.js

---
+++ controller.js
@@ -0,0 +1,8 @@
+const images = await Promise.all(
+    req.files.map(async (file) => {
+      return await uploadFile({
+        buffer: file.buffer,
+        fileName: file.originalname,
+      });
+    }),
+  );
